55fb7bf23af49b41f0e302940882f5881d8dfdda
user <user@DESKTOP-5351BE1>
2019-12-16 21:54:25 +0900

    2개의 값을 입력 받아 사칙연산 계산

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
 
 public class Calculator {
 	public static void main(String[] args) {
-		System.out.println("덧셈:" + (4 + 3));
-		System.out.println("뺄셈:" + (4 - 3));
-		System.out.println("곱셈:" + (4 * 3));
-		System.out.println("나눗셈:" + (4 / 2));
+
 		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("첫 번째 입력 값 :");
+		int first = sc.nextInt();
+		System.out.println("첫번째 수 : " + first);
+		
+		System.out.println("두 번째 입력 값 :");
+		int second = sc.nextInt();
+		System.out.println("두번째 수 : " + second);
+		
+		System.out.println("덧셈:" + (first + second));
+		System.out.println("뺄셈:" + (first - second));
+		System.out.println("곱셈:" + (first * second));
+		System.out.println("나눗셈:" + (first / second));
 	}
 }
