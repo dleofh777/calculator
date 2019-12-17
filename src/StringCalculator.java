eab7aebfcd05c323c7a051224a273f8d77cc98d8
user <user@DESKTOP-5351BE1>
2019-12-17 19:11:41 +0900

    문자열로 값을 받아 구현

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,28 +1,7 @@
 import java.util.Scanner;
 
 public class StringCalculator {
-		
-//		static int calculate(int first, String symbol, int second) {
-//			int result = 0;
-//			if(symbol.equals("+")) {
-//				result = first + second;
-//				System.out.println("덧셈 : " + result);
-//			}else if(symbol.equals("-")) {
-//				result = first - second;
-//				System.out.println("뺄셈 : " + result);
-//			}else if(symbol.equals("*")) {
-//				result = first * second;
-//				System.out.println("곱셈 : " + result);
-//			}else if(symbol.equals("/")) {
-//				result = first / second;
-//				System.out.println("나눗셈 : " + result);
-//			}else {
-//				System.out.println("잘못된 기호 입니다.");
-//				System.out.println("올바른 기호를 다시 입력해주세요.");
-//			}	
-//			return result;
-//		}
-				
+			
 		public static void main(String[] args) {	
 			Scanner sc = new Scanner(System.in);
 			System.out.println("계산할 값 입력 : ");
@@ -35,7 +14,7 @@ public class StringCalculator {
 			
 			int result = first;
 			
-			for(int i =1; i < values.length; i= i +2) {
+			for(int i =1; i < values.length; i +=2) {
 
 				String symbol = values[i];			
 				int second = Integer.parseInt(values[i+1]);
